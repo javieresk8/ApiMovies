96c1ce28294ff79c4177bf782aade371d467575f
Javier Erazo <alex.erazo01@epn.edu.ec>
2023-03-22 08:56:53 -0500

    add: category entity

---
+++ ApiMovies/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ApiMovies.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiMovies.Data
+{
+	public class ApplicationDbContext: DbContext
+	{
+		// Inyección de dependencias, herede todos los méthods en el constructor
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+		{
+
+		}
+
+		// Aqui debemos tener cada uno de nuestros modelos
+		// Desde aquí toma las clases para realizar las migraciones
+		public DbSet<Category> Category { get; set; }
+
+	}
+}
+
---
+++ ApiMovies/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiMovies.Models
+{
+	public class Category
+	{
+		// Importado de DataAnnotations
+		// ** Necesariamente necesitamos ponerle el nombre 'Id' para que sea
+		// auto incremental**
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+
+		public DateTime CreationDate { get; set; }
+	}
+}
+
